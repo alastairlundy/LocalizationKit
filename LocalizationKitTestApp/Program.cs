4e40002a55f8070e3be582ec3bd5c08c485ee691
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-07-10 23:59:12 +0100

    update test code

--- LocalizationKitTestApp/Program.cs
+++ LocalizationKitTestApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AlastairLundy.LocalizationKit;
+using AlastairLundy.SettingsKit;
 
 LocalizationManager localizationManager = new LocalizationManager();
 
-localizationManager.LoadLocalization("en_us", Environment.CurrentDirectory + Path.DirectorySeparatorChar + "locales" + Path.DirectorySeparatorChar + "en_us.json");
+localizationManager.LoadLocalization("en_us", Environment.CurrentDirectory + Path.DirectorySeparatorChar + "locales" + Path.DirectorySeparatorChar + "en_us.json", new JsonSettingsProvider<string, string>());
 
 var localization = localizationManager.GetLocalization("en_us");
 
-Console.WriteLine(localization.Translations.Get("chocolate").Value);
+Console.WriteLine(localization.GetLocalizedPhrase("earth.flatness").Value);
