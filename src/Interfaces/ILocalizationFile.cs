1d7a605a0e2dbbfcc00f11f5d0fb0751cfdb6447
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-01-31 23:42:10 +0000

    start de-coupling LocalizationKit from SettingsKit

---
+++ src/Interfaces/ILocalizationFile.cs
@@ -0,0 +1,23 @@
+﻿using AlastairLundy.SettingsKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlastairLundy.LocalizationKit.Interfaces
+{
+    public interface ILocalizationFile
+    {
+        public ILocalizationFileProvider SettingsProvider { get; }
+
+        public void Add(KeyValuePair<string, string> pair);
+
+        public void Remove(KeyValuePair<string, string> pair);
+
+        public KeyValuePair<string, string>[] Get();
+        public string Get(string key);
+
+        internal void SaveFile();
+    }
+}
---
+++ src/Interfaces/ILocalizationFileProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlastairLundy.LocalizationKit.Interfaces
+{
+    public interface ILocalizationFileProvider
+    {
+        KeyValuePair<string, string>[] Get(string pathToFile);
+
+        void WriteToFile(KeyValuePair<string, string>[] data, string pathToFile);
+    }
+}
