fc48b78f65979d5a84e2e376fb006f5730e98b59
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-02-01 00:38:37 +0000

    start work on localization file providers

---
+++ src/Providers/JsonLocalizationFileProvider.cs
@@ -0,0 +1,22 @@
+﻿using AlastairLundy.LocalizationKit.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlastairLundy.LocalizationKit.Providers
+{
+    public class JsonLocalizationFileProvider : ILocalizationFileProvider
+    {
+        public KeyValuePair<string, string>[] Get(string pathToFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteToFile(KeyValuePair<string, string>[] data, string pathToFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
