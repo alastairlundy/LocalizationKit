8195763fe2d208cf12aa5e5d5b46ee57fc7bc281
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-02-05 12:44:57 +0000

    add implementation code for JsonLocalizationFileProvider

--- src/Providers/JsonLocalizationFileProvider.cs
+++ src/Providers/JsonLocalizationFileProvider.cs
@@ -20,7 +20,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 using LocalizationKit.Interfaces;
 
 namespace LocalizationKit.Providers
@@ -29,12 +34,56 @@ namespace LocalizationKit.Providers
     {
         public KeyValuePair<string, string>[] Get(string pathToFile)
         {
-            throw new NotImplementedException();
+                List<KeyValuePair<string, string>> list = new();
+
+                string jsonText = File.ReadAllText(pathToFile);
+                jsonText = jsonText.Replace("{", String.Empty)
+                    .Replace("}", String.Empty)
+                    .Replace(",", String.Empty)
+                    .Replace('"', String.Empty[0])
+                    .Replace("'", String.Empty);
+
+                string[] lines = jsonText.Split("\r\n");
+
+                foreach (var line in lines)
+                {
+                    var newLine = line.Replace(" ", String.Empty);
+                    var splitLine = newLine.Split(':');
+                    
+                    list.Add(new KeyValuePair<string, string>(splitLine[0], splitLine[1]));
+                }
+
+                return list.ToArray();
         }
 
         public void WriteToFile(KeyValuePair<string, string>[] data, string pathToFile)
         {
-            throw new NotImplementedException();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append('{');
+            stringBuilder.AppendLine();
+            
+            foreach (var pair in data)
+            {
+                stringBuilder.Append('"');
+                stringBuilder.Append(pair.Key);
+                stringBuilder.Append('"');
+                
+                stringBuilder.Append(' ');
+                stringBuilder.Append(':');
+                stringBuilder.Append(' ');
+
+                stringBuilder.Append('"');
+                stringBuilder.Append(pair.Value);
+                stringBuilder.Append('"');
+                stringBuilder.Append(',');
+                stringBuilder.AppendLine();
+            }
+
+            stringBuilder.Append('}');
+            stringBuilder.AppendLine();
+            
+            File.WriteAllText(pathToFile, stringBuilder.ToString());
         }
     }
 }
