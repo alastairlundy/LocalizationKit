393f167257efd2106a9ce738d6100a2386ed49ba
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-02-20 20:27:42 +0000

    change spacing of conditional statements

--- src/Locale.cs
+++ src/Locale.cs
@@ -62,7 +62,7 @@ public class Locale
     {
         try
         {
-           #if NET6_0_OR_GREATER
+#if NET6_0_OR_GREATER
             if (locale.Contains('_'))
             {
                 var adjustedLocale = locale.Split('_');
@@ -85,7 +85,7 @@ public class Locale
                     CountryCode = adjustedLocale[1].ToLower();
                 }
             }
-            #else
+#else
             if (locale.Contains("_"))
             {
                 var adjustedLocale = locale.Split('_');
@@ -108,7 +108,7 @@ public class Locale
                     CountryCode = adjustedLocale[1].ToLower();
                 }
             }
-            #endif
+#endif
             else
             {
                 if(locale.Length == 2)
