a85f9cbec6a40b33a8c2a779c393facb9943fbba
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-02-21 14:36:39 +0000

    remove unneeded using namespaces

--- src/LocalizationManager.cs
+++ src/LocalizationManager.cs
@@ -21,10 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using LocalizationKit.Interfaces;
 
 namespace LocalizationKit{
     /// <summary>
